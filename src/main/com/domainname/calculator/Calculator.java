62bf98dde137a0f81c44b63c023a388c87c98798
Ella <ella@Ellas-MacBook-Pro.local>
2023-02-20 04:02:12 +0000

    add method

---
+++ src/main/com/domainname/calculator/Calculator.java
@@ -0,0 +1,7 @@
+package com.domainname.calculator;
+
+public class Calculator {
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+}
---
+++ src/test/com/domainname/calculator/CalculatorTest.java
@@ -0,0 +1,29 @@
+package com.domainname.calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calc;
+
+    @BeforeEach
+    void setUp() {
+        calc = new Calculator();
+    }
+
+    @Test
+    public void canAddZeroPlusZero(){
+        int sum = calc.add(0, 0);
+        assertEquals(0, sum);
+    }
+
+    @Test
+    public void canAddOnePlusOne(){
+        int sum = calc.add(1, 1);
+        assertEquals(2, sum);
+    }
+
+}
